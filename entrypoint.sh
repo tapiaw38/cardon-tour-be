91ffcd518cf28fbada0021ca7956704464dd5f3c
tapiaw38 <tapiaw38@gmail.com>
2024-08-25 01:45:16 -0300

    Update init app mode local

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Wait for PostgreSQL to be ready
+until pg_isready --host=cardon-postgres-db --port=5432 --username=postgres --dbname=cardon-tour-db
+do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 2
+done
+
+# PostgreSQL is now ready, to run the Go application
+echo "========== Starting Go application =========="
+exec go run cmd/api/main.go --host 0.0.0.0
